428b5fb9c81ba0b4655089654fd9a38c95670c08
Ashkan <ashkan_ahrari@yahoo.com>
2025-06-11 10:35:27 +0300

    add some changes to metadata of templates, add a summary to model

--- myApp/models.py
+++ myApp/models.py
@@ -9,7 +9,8 @@ class Project(models.Model):
     DRAFT = 'DF', 'Draft'
 
   title = models.CharField(max_length=264)
-  description = models.CharField(max_length=264)
+  summary = models.CharField(max_length=255)
+  description = models.CharField(max_length=500)
   image = models.ImageField(upload_to='uploads/')
   address = models.URLField()
   status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
@@ -17,10 +18,18 @@ class Project(models.Model):
   meta_title = models.CharField(max_length=70, blank=True)
   meta_description = models.CharField(max_length=160, blank=True)
 
-  def save(self, *args, **kwargs):
-      if not self.slug:
-          self.slug = slugify(self.title)
-      super().save(*args, **kwargs)
+def save(self, *args, **kwargs):
+  if not self.slug:
+    self.slug = slugify(self.title)
+
+  if not self.meta_title:
+    self.meta_title = self.title[:70]
+
+  if not self.meta_description:
+    self.meta_description = (self.summary or self.description)[:160]
+
+    super().save(*args, **kwargs)
+
 
   def __str__(self):
     return self.title
